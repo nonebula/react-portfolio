cdfb53a38a44172275b0a9ff15f302f1235854d9
nonebula <147166151+nonebula@users.noreply.github.com>
2024-01-30 12:27:03 +0000

    json files and data folder created & populated with placeholders

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from "./About.module.css"
+import styles from "./Skills.module.css"
 import { getImageUrl } from '../../utils'
 
 
-export const About = () => {
+export const Skills = () => {
   return (
     <section className={styles.container}>
   
