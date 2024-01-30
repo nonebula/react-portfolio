115b261a191963ead471b075505dc0f4017ec73d
nonebula <147166151+nonebula@users.noreply.github.com>
2024-01-30 10:41:28 +0000

    Setup Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
-  return <>Hello world!</>;
+  return <div className={styles.App}>Hello world!</div>;
 }
 
 export default App
